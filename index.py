fd0671a42f5fbeae87aca211b844a11c7ad8e88e
Toño <sr.periky@gmail.com>
2023-05-23 13:44:33 -0500

    ya se implementó las vistas, solo falta la lógica del programa

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QPushButton, QHBoxLayout, QWidget, QLabel, QLineEdit, QVBoxLayout, QComboBox, QStackedWidget
+from PyQt5.QtWidgets import QApplication, QPushButton, QHBoxLayout, QWidget, QLabel, QLineEdit, QVBoxLayout, QComboBox, QStackedWidget, QTextEdit
 from PyQt5.QtGui import QColor
 from PyQt5 import QtCore
 import webbrowser
@@ -10,8 +10,11 @@ class MainWindow(QWidget):
         self.stacked_widget = QStackedWidget()
         self.layout = QVBoxLayout(self)
         self.layout.addWidget(self.stacked_widget)
-
         self.show_home_view()
+
+    ############
+    #  DISEÑO  # 
+    ############
 
     def show_home_view(self):
         home_view = QWidget()
@@ -26,7 +29,6 @@ class MainWindow(QWidget):
         tutorial_button.setStyleSheet("background-color: #00BD9D; color: white;")
         credits_button = QPushButton("Créditos")
         credits_button.setStyleSheet("background-color: #00BD9D; color: white;")
-
         buttons_layout.addWidget(credits_button)
         buttons_layout.addWidget(tutorial_button)
         buttons_layout.addWidget(info_button)
@@ -37,17 +39,17 @@ class MainWindow(QWidget):
         title_label.setAlignment(QtCore.Qt.AlignCenter)
 
         # Input
-        input_line_edit = QLineEdit()
-        input_line_edit.setStyleSheet("background-color: white; border: 1px solid #ccc; padding: 5px; ")
+        self.input_line_edit = QLineEdit()
+        self.input_line_edit.setStyleSheet("background-color: white; border: 1px solid #ccc; padding: 5px; ")
 
         # Selector
-        selector_combobox = QComboBox()
-        selector_combobox.addItem("Metodo 1")
-        selector_combobox.addItem("Metodo 2")
-        selector_combobox.addItem("Metodo 3")
-        selector_combobox.setStyleSheet("background-color: white;color: black; border: 1px solid #ccc;  padding: 5px; selection-background-color: #00BD9D;")
-        selector_combobox.setSizeAdjustPolicy(QComboBox.AdjustToContents)
-        selector_combobox.view().setStyleSheet("color: black;")
+        self.selector_combobox = QComboBox()
+        self.selector_combobox.addItem("Metodo 1")
+        self.selector_combobox.addItem("Metodo 2")
+        self.selector_combobox.addItem("Metodo 3")
+        self.selector_combobox.setStyleSheet("background-color: white;color: black; border: 1px solid #ccc;  padding: 5px; selection-background-color: #00BD9D;")
+        self.selector_combobox.setSizeAdjustPolicy(QComboBox.AdjustToContents)
+        self.selector_combobox.view().setStyleSheet("color: black;")
 
         # Botón Resolver
         solve_button = QPushButton("Resolver")
@@ -56,10 +58,9 @@ class MainWindow(QWidget):
         # Agregar widgets al diseño
         layout.addLayout(buttons_layout)
         layout.addWidget(title_label)
-        layout.addWidget(input_line_edit)
-        layout.addWidget(selector_combobox)
+        layout.addWidget(self.input_line_edit)
+        layout.addWidget(self.selector_combobox)
         layout.addWidget(solve_button)
-
         home_view.setLayout(layout)
         self.stacked_widget.addWidget(home_view)
 
@@ -67,12 +68,10 @@ class MainWindow(QWidget):
         info_button.clicked.connect(self.show_info_view)
         tutorial_button.clicked.connect(self.show_tutorial_view)
         credits_button.clicked.connect(self.show_credits_view)
-        solve_button.clicked.connect(self.solve)
+        solve_button.clicked.connect(self.show_solution_view)
 
         # Mostrar la vista principal
         self.stacked_widget.setCurrentWidget(home_view)
-
-
 
     def show_tutorial_view(self):
         webbrowser.open("https://www.youtube.com/watch?v=lo1PzjuGxFg")
@@ -91,7 +90,6 @@ class MainWindow(QWidget):
         back_button.setStyleSheet("background-color: #00BD9D; color: white;")
         layout.addWidget(back_button, alignment=QtCore.Qt.AlignCenter)
         back_button.clicked.connect(self.show_home_view)
-
         info_view.setLayout(layout)
         self.stacked_widget.addWidget(info_view)
 
@@ -112,12 +110,66 @@ class MainWindow(QWidget):
         back_button.setStyleSheet("background-color: #00BD9D; color: white;")
         layout.addWidget(back_button, alignment=QtCore.Qt.AlignCenter)
         back_button.clicked.connect(self.show_home_view)
-
         credits_view.setLayout(layout)
         self.stacked_widget.addWidget(credits_view)
 
         # Mostrar la vista de créditos
         self.stacked_widget.setCurrentWidget(credits_view)
+
+    def show_solution_view(self):
+        method = self.selector_combobox.currentText()
+        text = self.input_line_edit.text()
+        solution_view = QWidget()
+        solution_view.setStyleSheet("background-color: #FFFBFA;")
+        layout = QVBoxLayout()
+
+        # Título
+        title_label = QLabel("Solución")
+        title_label.setStyleSheet("color: #BF1363; font-size: 24px; font-weight: bold;")
+        title_label.setAlignment(QtCore.Qt.AlignCenter)
+
+        # Información de la ecuación
+        equation_label = QLabel(f"Ecuación: {text}\nSí es una ecuación diferencial")
+        equation_label.setStyleSheet("color: black; font-size: 16px;")
+
+        # Detalles de la ecuación
+        details_label = QLabel("* Orden: 2\n* Tipo: Homogénea\n* Grado de homogeneidad: 3")
+        details_label.setStyleSheet("color: black; font-size: 16px;")
+
+        # Solución
+        solution_label = QLabel(f"Resultado de la ecuación con el {method}\n")
+        solution_label.setStyleSheet("color: black; font-size: 16px;")
+
+       # Área de texto para mostrar la solución
+        solution_text_edit = QTextEdit("123... aquí la solucion")
+        solution_text_edit.setStyleSheet("background-color: white; border: 1px solid #ccc; padding: 5px;")
+        solution_text_edit.setFixedHeight(3 * solution_text_edit.fontMetrics().height())
+        solution_text_edit.setReadOnly(True)  # Deshabilitar la edición del texto
+
+        # Botón Regresar
+        back_button = QPushButton("Regresar")
+        back_button.setStyleSheet("background-color: #00BD9D; color: white;")
+
+        # Conexión del botón Regresar
+        back_button.clicked.connect(self.show_home_view)
+
+        # Agregar widgets al diseño
+        layout.addWidget(title_label)
+        layout.addWidget(equation_label)
+        layout.addWidget(details_label)
+        layout.addWidget(solution_label)
+        layout.addWidget(solution_text_edit)
+        layout.addWidget(back_button)
+        solution_view.setLayout(layout)
+        self.stacked_widget.addWidget(solution_view)
+
+        # Mostrar la vista de solución
+        self.stacked_widget.setCurrentWidget(solution_view)
+
+
+    ##############
+    #   Lógica   #
+    ##############
 
     def solve(self):
         # Lógica para resolver el problema con el método seleccionado
